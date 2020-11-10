44acbf8b2f4dfa9a589d21dbbc22bab0c9ac6736
Nick Avtges <navtges3@gmail.com>
2020-11-10 10:48:53 -0500

    I fixed the whole thing. might come back for validation cases

--- HW6/js/myScript.js
+++ HW6/js/myScript.js
@@ -1,48 +1,101 @@
+//This function will generate the base table
+function startup() {
+  var startx = 1;
+  var endx = 10;
+  var starty = 1;
+  var endy = 10;
+
+  var table = document.createElement('table');
+  table.id = "myTable";
+
+  for (var i = starty - 1; i <= endy; i++){
+    var tr = document.createElement('tr');
+    //Inner loop to generate columns
+    for (var j = startx - 1; j <= endx; j++){
+      var val;
+      if(i == starty - 1) {
+        val = j;
+      }
+      else if(j == startx - 1) {
+        val = i;
+      }
+      else{
+        val = i * j;
+      }
+      //Create the cells and add them to the table
+      var td = document.createElement('td');
+      var text = document.createTextNode(val);
+      td.appendChild(text);
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  document.body.appendChild(table);
+}
+
 //This function is used to generate a table given four values
 function myFunction() {
   //Get the values from the form
-  var title = "Results";
-  var valid = true;
-  var numbers = new Array(5);
-  numbers[0] = 1;
-  numbers[1] = document.forms["myForm"]["n1"].value;
-  numbers[2] = document.forms["myForm"]["n2"].value;
-  numbers[3] = document.forms["myForm"]["n3"].value;
-  numbers[4] = document.forms["myForm"]["n4"].value;
+  var numbers = new Array(4);
+  numbers[0] = document.forms["myForm"]["n1"].value;
+  numbers[1] = document.forms["myForm"]["n2"].value;
+  numbers[2] = document.forms["myForm"]["n3"].value;
+  numbers[3] = document.forms["myForm"]["n4"].value;
 
-  for(var x = 1; x < 5; x++){
+//if nothing is sumbitted
+  for(var x = 0; x < 4; x++){
     if(numbers[x] == null || numbers[x] == ""){
-      valid = false;
-      break;
+      if(x == 0 || x == 2)
+        numbers[x] = 1;
+      else {
+        numbers[x] = 10;
+      }
     }
   }
 
-  if(valid){
-    //Generate the table
-    var table = document.createElement('table');
-    table.id = "myTable";
-    //Outer loop to generate rows
-    for (var i = 0; i < 5; i++){
-      var tr = document.createElement('tr');
-      //Inner loop to generate columns
-      for (var j = 0; j < 6; j++){
-        var val;
-        //Make sure that the values stored in numbers are printed properly
-        if(j == 0){
-          val = numbers[i];
-        }
-        //Calculate the rest of the table values
-        else{
-          val = numbers[i] * j;
-        }
-        //Create the cells and add them to the table
-        var td = document.createElement('td');
-        var text = document.createTextNode(val);
-        td.appendChild(text);
-        tr.appendChild(td);
-      }
-      table.appendChild(tr);
-    }
-    document.body.appendChild(table);
+  // swaps if start is bigger than end
+  if(numbers[0] > numbers[1]){
+    var temp = numbers[0];
+    numbers[0] = numbers[1];
+    numbers[1] = temp;
   }
+  if(numbers[2] > numbers[3]){
+    var temp = numbers[2];
+    numbers[2] = numbers[3];
+    numbers[3] = temp;
+  }
+
+  //get rid of old Table
+  var elem = document.getElementById('myTable');
+  if(typeof elem != 'undefined'){
+    elem.parentNode.removeChild(elem);
+  }
+
+  //make a new table
+  var table = document.createElement('table');
+  table.id = "myTable";
+
+  for (var i = numbers[2] - 1; i <= numbers[3]; i++){
+    var tr = document.createElement('tr');
+    //Inner loop to generate columns
+    for (var j = numbers[0] - 1; j <= numbers[1]; j++){
+      var val;
+      if(i == numbers[2] - 1) {
+        val = j;
+      }
+      else if(j == numbers[0] - 1) {
+        val = i;
+      }
+      else{
+        val = i * j;
+      }
+      //Create the cells and add them to the table
+      var td = document.createElement('td');
+      var text = document.createTextNode(val);
+      td.appendChild(text);
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  document.body.appendChild(table);
 }
